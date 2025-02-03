fe505ba4d57c1f871698ee86518f730246b25e5d
Jurijs Isats <mansvards@hotmail.com>
2025-02-03 22:51:52 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace SoundTest4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.RightVolumeSlider05 = new System.Windows.Forms.TrackBar();
+            this.stop05 = new System.Windows.Forms.Button();
+            this.start05 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RightVolumeSlider05)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.RightVolumeSlider05);
+            this.groupBox1.Controls.Add(this.stop05);
+            this.groupBox1.Controls.Add(this.start05);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(623, 161);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Start, stop, increase volume";
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(445, 19);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(31, 17);
+            this.radioButton10.TabIndex = 12;
+            this.radioButton10.Text = "6";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            this.radioButton10.CheckedChanged += new System.EventHandler(this.radioButton10_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(316, 19);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 11;
+            this.radioButton9.Text = "4";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(562, 19);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(37, 17);
+            this.radioButton8.TabIndex = 10;
+            this.radioButton8.Text = "10";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(503, 19);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 9;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(380, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.Text = "5";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(249, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.Text = "3";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(187, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "2";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Checked = true;
+            this.radioButton3.Location = new System.Drawing.Point(128, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "1";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(66, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(40, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "0.5";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(7, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(40, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.Text = "0.2";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // RightVolumeSlider05
+            // 
+            this.RightVolumeSlider05.LargeChange = 1;
+            this.RightVolumeSlider05.Location = new System.Drawing.Point(131, 19);
+            this.RightVolumeSlider05.Maximum = 11;
+            this.RightVolumeSlider05.Minimum = 1;
+            this.RightVolumeSlider05.Name = "RightVolumeSlider05";
+            this.RightVolumeSlider05.Size = new System.Drawing.Size(486, 45);
+            this.RightVolumeSlider05.TabIndex = 2;
+            this.RightVolumeSlider05.Value = 1;
+            this.RightVolumeSlider05.Scroll += new System.EventHandler(this.RightVolumeSlider05_Scroll);
+            this.RightVolumeSlider05.ValueChanged += new System.EventHandler(this.RightVolumeSlider05_ValueChanged);
+            // 
+            // stop05
+            // 
+            this.stop05.Location = new System.Drawing.Point(64, 19);
+            this.stop05.Name = "stop05";
+            this.stop05.Size = new System.Drawing.Size(49, 23);
+            this.stop05.TabIndex = 1;
+            this.stop05.Text = "stop";
+            this.stop05.UseVisualStyleBackColor = true;
+            this.stop05.Click += new System.EventHandler(this.stop05_Click);
+            // 
+            // start05
+            // 
+            this.start05.Location = new System.Drawing.Point(6, 19);
+            this.start05.Name = "start05";
+            this.start05.Size = new System.Drawing.Size(52, 23);
+            this.start05.TabIndex = 0;
+            this.start05.Text = "start";
+            this.start05.UseVisualStyleBackColor = true;
+            this.start05.Click += new System.EventHandler(this.start05_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(184, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(276, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(322, 44);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(368, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(413, 44);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(459, 44);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(13, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "8";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(505, 44);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(13, 13);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "9";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(549, 44);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(19, 13);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "10";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(593, 44);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(19, 13);
+            this.label11.TabIndex = 23;
+            this.label11.Text = "11";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButton5);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton3);
+            this.groupBox2.Controls.Add(this.radioButton4);
+            this.groupBox2.Controls.Add(this.radioButton6);
+            this.groupBox2.Controls.Add(this.radioButton7);
+            this.groupBox2.Controls.Add(this.radioButton8);
+            this.groupBox2.Controls.Add(this.radioButton9);
+            this.groupBox2.Controls.Add(this.radioButton10);
+            this.groupBox2.Location = new System.Drawing.Point(9, 93);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(607, 61);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Frequency in Khz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 182);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Right Ear Hearing Test";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RightVolumeSlider05)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TrackBar RightVolumeSlider05;
+        private System.Windows.Forms.Button stop05;
+        private System.Windows.Forms.Button start05;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+
+namespace SoundTest4
+{
+    public partial class Form1 : Form
+    {
+        private WaveOutEvent waveOutLeft;
+        private WaveOutEvent waveOutRight;
+        private SignalGenerator signalGeneratorLeft;
+        private SignalGenerator signalGeneratorRight;
+        private double frequency = 1000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            stop05.Enabled = false; // Disable the stop button initially
+        }
+
+        private void start05_Click(object sender, EventArgs e)
+        {
+            stop05_Click(sender, e); // Stop any existing playback
+            stop05.Enabled = true; // Enable the stop button
+            start05.Enabled = false; // Disable the start button
+
+            // Initialize the signal generator for the right channel
+            signalGeneratorRight = new SignalGenerator(44100, 1)
+            {
+                Gain = 0.2,
+                Frequency = frequency,
+                Type = SignalGeneratorType.Sin
+            };
+
+            // Create a stereo sample provider with silence in the left channel
+            var stereo = new MonoToStereoSampleProvider(signalGeneratorRight);
+            stereo.LeftVolume = 0.0f; // Silence in left channel
+            stereo.RightVolume = (float)RightVolumeSlider05.Value / 50; // Volume in right channel
+
+            // Initialize and play the wave output for the right channel
+            waveOutRight = new WaveOutEvent();
+            waveOutRight.Init(stereo);
+            waveOutRight.Play();
+        }
+
+        private void stop05_Click(object sender, EventArgs e)
+        {
+            stop05.Enabled = false; // Disable the stop button
+            start05.Enabled = true; // Enable the start button
+            waveOutRight?.Stop(); // Stop playback
+            waveOutRight?.Dispose(); // Dispose of the wave output
+        }
+
+        private void RightVolumeSlider05_Scroll(object sender, EventArgs e)
+        {
+            if (signalGeneratorRight != null)
+            {
+                // Adjust the gain of the signal generator based on the slider value
+                signalGeneratorRight.Gain = RightVolumeSlider05.Value / 50;
+            }
+        }
+
+        private void HandleRadioButtonCheckedChanged(RadioButton radioButton, int frequencyValue)
+        {
+            if (radioButton.Checked)
+            {
+                // Update the frequency and reset the volume slider
+                frequency = frequencyValue;
+                RightVolumeSlider05.Value = RightVolumeSlider05.Minimum;
+            }
+        }
+
+        // Event handlers for radio button checked changes
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton1, 200);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton2, 500);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton3, 1000);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton4, 2000);
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton5, 3000);
+        }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton9, 4000);
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton6, 5000);
+        }
+
+        private void radioButton10_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton10, 6000);
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton7, 7000);
+        }
+
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            HandleRadioButtonCheckedChanged(radioButton8, 10000);
+        }
+
+        private void RightVolumeSlider05_ValueChanged(object sender, EventArgs e)
+        {
+            start05_Click(sender, e); // Restart playback with the new volume
+        }
+    }
+}
